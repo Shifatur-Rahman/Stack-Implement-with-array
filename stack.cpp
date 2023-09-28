b8c228137864c6f818a0d570d77ef3fcf975bf31
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2023-09-28 16:53:18 +0600

    Add files via upload
    #Stack Implement with array in c++

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// main function
+int main(){
+
+// int myStack[stackSize];
+stack<int> myStack;
+int size,n;
+cin>>size;
+
+for(int i=0; i<size; i++){
+     cin>>n
+     myStack.push(n);
+}
+
+
+
+
+
+
+}
